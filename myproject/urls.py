9c7bbea46db2cfb709486cf5403efce1ff1ae4db
aigerim678 <aigerimsabyrkhan393@gmail.com>
2024-12-07 16:51:38 +0500

    Requests and Responses

---
+++ myproject/urls.py
@@ -0,0 +1,10 @@
+from django.urls import include, path
+from rest_framework import routers
+
+
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
+urlpatterns = [
+    path('', include('snippets.urls')),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+]
